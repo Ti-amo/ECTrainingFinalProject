d66c20ee43d14e0cec144ac9ded74fcba303b524
Ti-amo <thehungklk@gmail.com>
2021-11-29 14:43:33 +0700

    create Entity & DAO class

---
+++ EmployeeManagement/EmployeeManagement/DAO/EmployeeDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EmployeeManagement.Entity;
+
+namespace EmployeeManagement.DAO
+{
+    public class EmployeeDAO
+    {
+        public EmployeeDAO() { }
+        public List<EmployeeEntity> FindAll()
+        {
+            List<EmployeeEntity> listEmployee = new List<EmployeeEntity>();
+            return listEmployee;
+        }
+        public EmployeeEntity Find(string pkey)
+        {
+            EmployeeEntity employeeEntity = new EmployeeEntity();
+            return employeeEntity;
+        }
+        public int Insert(EmployeeEntity entity)
+        {
+            int result = 0;
+            return result;
+        }
+        public int Update(EmployeeEntity entity)
+        {
+            int result = 0;
+            return result;
+        }
+        public int Delete(EmployeeEntity entity)
+        {
+            int result = 0;
+            return result;
+        }
+    }
+}
