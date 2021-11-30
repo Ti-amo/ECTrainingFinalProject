1225ececd8877743d88fb3bfe19a4c5bcc4b1bf4
thuyquynhcao <38751619+thuyquynhcao@users.noreply.github.com>
2021-11-30 09:06:15 +0700

    up

---
+++ EmployeeManagement/EmployeeManagement/Finish.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeeManagement {
+    public partial class Finish : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void ButtonBack_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
