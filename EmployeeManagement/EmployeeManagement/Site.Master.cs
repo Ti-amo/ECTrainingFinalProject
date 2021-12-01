5f82b35b7829de31836acbdfc4b0c48453b9cb8a
Ti-amo <thehungklk@gmail.com>
2021-12-01 11:05:47 +0700

    config header for login status

--- EmployeeManagement/EmployeeManagement/Site.Master.cs
+++ EmployeeManagement/EmployeeManagement/Site.Master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,30 @@ namespace EmployeeManagement
 {
     public partial class Site : System.Web.UI.MasterPage
     {
+        public bool login = false;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["login"] != null)
+            {
+                UserEntity user = (UserEntity)Session["user"];
+                login = true;
+                int date = int.Parse(DateTime.Now.ToString("HH", System.Globalization.DateTimeFormatInfo.InvariantInfo));
+                string msg;
+                if (date > 0 && date <= 10)
+                {
+                    msg = "おはようございます、";
+                } else if (date > 10 && date <= 17)
+                {
+                    msg = "こんにちは、";
+                } else msg = "こんばんは、";
+                LabelHello.Text = msg + user.UserId;
+            }
+        }
 
+        protected void ButtonLogout_Click(object sender, EventArgs e)
+        {
+            Session.Remove("login");
+            Response.Redirect(@"Logout.aspx");
         }
     }
 }
