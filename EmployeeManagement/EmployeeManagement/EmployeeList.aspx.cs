c32290edfdde5dfab13c9f15e87d8e0b2d96a635
honghanh2608 <nguyenthihonghanh2608@gmail.com>
2021-12-01 13:23:06 +0700

    fix conflic

--- EmployeeManagement/EmployeeManagement/EmployeeList.aspx.cs
+++ EmployeeManagement/EmployeeManagement/EmployeeList.aspx.cs
@@ -8,20 +8,20 @@ namespace EmployeeManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-/*            List<LicenseEntity> licenses = new List<LicenseEntity>();
+            List<LicenseEntity> licenses = new List<LicenseEntity>();
             licenses.Add(new LicenseEntity("", "FE", ""));
             licenses.Add(new LicenseEntity("", "MCITP", ""));
             List<EmployeeEntity> employees = new List<EmployeeEntity>();
-            employees.Add(new EmployeeEntity("20166061", "山田太郎", "やまだたろう", "男", "2000/02/20", licenses, "人事部", "2000/02/20"));
-            employees.Add(new EmployeeEntity("20166061", "山田太郎", "やまだたろう", "男", "2000/02/20", licenses, "人事部", "2000/02/20"));
+            employees.Add(new EmployeeEntity("20166061", "山田太郎", "やまだたろう", "男", licenses, "人事部", "2000/02/20"));
+            employees.Add(new EmployeeEntity("20166061", "山田太郎", "やまだたろう", "男", licenses, "人事部", "2000/02/20"));
             RepeaterEmployeeList.DataSource = employees;
-            RepeaterEmployeeList.DataBind();*/
-            
+            RepeaterEmployeeList.DataBind();
+
         }
 
         protected void ButtonUpdate_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("EmployeeUpdate.aspx");
         }
 
         protected void ButtonDelete_Click(object sender, EventArgs e)
